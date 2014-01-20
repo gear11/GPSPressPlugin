37ae85acb5347ab7cb3d0e2d59cedce70682a196
Andy Jenkins <andrewbjenkins@gmail.com>
2014-01-20 00:06:51 -0500

    Add initial plugin based on Hello.php

---
+++ gps-press/gps-press.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * @package GPSPress
+ * @version 1.6
+ */
+/*
+Plugin Name: GPSPress
+Plugin URI: http://gear11.com/gpspress/
+Description: Tag and browse posts by time and location.
+Version: 1.0.0
+Author: Gear 11
+Author URI: http://gear11.com
+License: GPL2
+*/
+/*  Copyright 2014  Gear 11, LLC
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+define('GPS_PRESS_VERSION', '1.0.0');
+define('GPS_PRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+function hello_dolly_get_lyric() {
+	/** These are the lyrics to Hello Dolly */
+	$lyrics = "Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+So, take her wrap, fellas
+Find her an empty lap, fellas
+Dolly'll never go away again
+Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+Golly, gee, fellas
+Find her a vacant knee, fellas
+Dolly'll never go away
+Dolly'll never go away
+Dolly'll never go away again";
+
+	// Here we split it into lines
+	$lyrics = explode( "\n", $lyrics );
+
+	// And then randomly choose a line
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+// This just echoes the chosen line, we'll position it later
+function hello_dolly() {
+	$chosen = hello_dolly_get_lyric();
+	echo "<p id='dolly'>GPSPress: $chosen</p>";
+}
+
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'hello_dolly' );
+
+// We need some CSS to position the paragraph
+function dolly_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
+	echo "
+	<style type='text/css'>
+	#dolly {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 11px;
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'dolly_css' );
+
+?>
---
+++ gps-press/readme.txt
@@ -0,0 +1,23 @@
+=== GPSPress ===
+Contributors: Gear11
+Tags: gps, location, map
+Requires at least: 3.8
+Tested up to: 3.8
+License: GPL V2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds time and location information to WordPress posts to support discovery
+by date and place.
+
+. With GPSPress, your readers can browse
+your posts by time and location.  Includes a widget for displaying your posts
+as markers in Google Maps.
+
+== Installation ==
+TBD
+
+== Frequently Asked Questions ==
+None yet
+
+== Changelog ==
+2014/1/19 Initial version
