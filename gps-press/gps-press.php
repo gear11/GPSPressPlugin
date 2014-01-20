f679aff9326e3d0d26eda0bfdae6b609feda8692
Andy Jenkins <andrewbjenkins@gmail.com>
2014-01-20 17:21:29 -0500

    Create simple gulp build file and add to Aptana autobuild.

--- gps-press/gps-press.php
+++ gps-press/gps-press.php
@@ -76,7 +76,7 @@ Dolly'll never go away again";
 // This just echoes the chosen line, we'll position it later
 function hello_dolly() {
 	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>GPSPress: $chosen</p>";
+	echo "<p id='dolly'>GPSPress Foo: $chosen</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
